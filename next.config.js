c0f3e501e7b3fce20e9709a3e771f82a4a01e482
unknown <eyescream16wind@proton.me>
2025-06-20 17:14:10 -0300

    fix: ajustar configurações do Next.js para módulos ES

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-const path = require('path');
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   images: {
     domains: ['localhost'],
     remotePatterns: [
@@ -16,22 +13,17 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname, 'src'),
-      '@/lib': path.resolve(__dirname, 'src/lib'),
-      '@/components': path.resolve(__dirname, 'src/components'),
-      '@/hooks': path.resolve(__dirname, 'src/hooks'),
-      '@/services': path.resolve(__dirname, 'src/services'),
-      '@/types': path.resolve(__dirname, 'src/types'),
-      '@/utils': path.resolve(__dirname, 'src/utils'),
+      '@': './src',
+      '@/lib': './src/lib',
+      '@/components': './src/components',
+      '@/hooks': './src/hooks',
+      '@/services': './src/services',
+      '@/types': './src/types',
+      '@/utils': './src/utils',
     };
-    
     return config;
   },
-  // Configurações para melhorar o build
   swcMinify: true,
   compress: true,
   poweredByHeader: false,
-}
-
-module.exports = nextConfig 
+}; 
